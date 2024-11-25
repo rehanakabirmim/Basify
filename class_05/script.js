5ee1d25ac2530baf9fb00eb9219f2f1710c3681a
rehanakabirmim <rehanakabirmim@gmail.com>
2024-11-25 20:58:45 +0600

    class 05 half completed

---
+++ class_05/script.js
@@ -0,0 +1,84 @@
+// console.log("(before ad");
+// let arr = ["a","b","c"];
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+//push()last e value add kore
+
+// console.log("(after ad");
+// arr.push("d");
+// arr.push(5);
+// for(let i=0;i<arr.length;i++)
+//     {
+//         console.log(arr[i]);//output a,b,c,d,5
+//     }
+
+// //pop()last theke delete kore
+// console.log("(after pop");
+// arr.pop();
+
+// for(let i=0;i<arr.length;i++)
+//     {
+//         console.log(arr[i]);//output a b c d niche niche
+
+//     }
+
+// //tostring() array r string er comma soho print kore
+// let names =['a','b','c'];
+// const result = names.toString();
+// console.log(result);//output a,b,c
+
+//shift
+// let s =[1,2,3,4]
+// const x = s.shift();
+// const y= s.shift();
+// // console.log(x);//output 1
+
+// for(let i=0;i<s.length;i++){
+//     console.log(s[i]);//output 2,3,4 //loop er moddhe shift()1st item remove kore
+
+
+// }
+
+
+// let s =[1,2,3,4]
+// const x = s.shift();
+// const y= s.shift();
+// // console.log(x);//output 1
+
+// for(let i=0;i<s.length;i++){
+//     console.log(s[i]);//output 3,4 //loop er moddhe shift()1st item remove kore
+
+
+// }
+
+
+// let s =[1,2,3,4,5]
+// const x = s.unshift();
+// console.log(x);//output 5 last value show korbe
+
+// for(let i=0;i<s.length;i++){
+//     console.log(s[i]);//output 1,2,3,4,5 //loop er moddhe unshift()1st item add kore
+
+    
+// }
+
+//concat()is used with arrays or strings, 
+// let x="a";
+// let y="b";
+// let res = x.concat(y);
+// console.log(res);
+
+ let s =[1,2,3,4,5]
+
+ let y =[2023]
+ let z=["ok"];
+
+ const res= s.concat(y).concat(z);
+for(let i=0;i<res.length;i++){
+    console.log(res[i]);//output 1,2,3,4,5 //loop er moddhe unshift()1st item add kore
+
+    
+}
