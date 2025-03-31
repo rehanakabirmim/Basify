d67b37813dade6e1a50c97453972e9d3272df99f
rehanakabirmim <rehanakabirmim@gmail.com>
2025-03-31 16:50:44 +0600

    test1

---
+++ assesment/test1.js
@@ -0,0 +1,15 @@
+// Declare variables
+let a = 10;
+let b = 5;
+
+// Perform operations
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+// Display results
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
